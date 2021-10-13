e0a5748c69e702e835d94508c7af6ce1b274db90
CBlumini <72533588+CBlumini@users.noreply.github.com>
2021-10-12 20:22:09 -0700

    defined frog methods

--- src/com/Frog/Frog.java
+++ src/com/Frog/Frog.java
@@ -1,5 +1,7 @@
 package com.Frog;
 
+import com.Fly.Fly;
+
 public class Frog {
     //instance variable
     private String name;
@@ -11,6 +13,7 @@ public class Frog {
     public static final int DEFAULT_AGE =5;
     public static final double DEFAULT_TONGUE_SPEED = 5;
 
+    //*******************Constructors******************
     //main constructor
     public Frog(String name, int age, double tongueSpeed){
         this.name = name;
@@ -31,11 +34,72 @@ public class Frog {
         //this.name = name;
     }
 
+    //*****************METHODS******************************
+    //getters
+    public static String getSpecies() {
+        return species;
+    }
 
+    //setters
+    public static void setSpecies(String species) {
+        Frog.species = species;
+    }
 
+    //toString
+    public String toString(){
+        if (isFroglet){
+            return "My name is " + name + " and I’m a rare froglet! I’m " + age + " months old and my tongue has a speed of " + tongueSpeed + ".";
+        } else {
+            return "My name is " + name + " and I’m a rare frog. I’m " + age + " months old and my tongue has a speed of " + tongueSpeed+ ".";
+        }
+    }
 
+    //Grow Method
+    public void grow(int addMonths){
+        for (int i = 0; i <= addMonths; i++) {
+           if (age<=12){
+               age++;
+               tongueSpeed++;
+               isFroglet = age > 1 && age < 7; //simplified if else statement that gives a boolean value to isFroglet
+           } else if (age<30){
+               age++;
+           } else if(tongueSpeed>5){
+               age++;
+               tongueSpeed--;
+           } else{
+               age++;
+           }
+        }
+    }
+    public void grow(){
+        age++;
+        if (age<=12){
+            tongueSpeed++;
+            isFroglet = age > 1 && age < 7; //simplified if else statement that gives a boolean value to isFroglet
+        } else if(age>30 && tongueSpeed>5){
+            tongueSpeed--;
+        }
+    }
+
+    //Eat Method
+    public void eat(Fly food){
+        if(food.isDead()){
+            return; //do I really need this?
+        } else if(food.getSpeed()<tongueSpeed){
+            if (food.getMass() >= age*0.5){
+                grow();
+                food.setMass(0);
+            }
+        } else {
+            food.grow(1);
+        }
+    }
+
+/*
 
     public static void main(String[] args) {
 
     }
+
+ */
 }
