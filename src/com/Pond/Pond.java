eedd91b6c7ba9488b90c45709742e89a192cd55d
CBlumini <72533588+CBlumini@users.noreply.github.com>
2021-10-14 19:05:33 -0700

    Updated froglet setting

--- src/com/Pond/Pond.java
+++ src/com/Pond/Pond.java
@@ -5,6 +5,9 @@ import com.Fly.Fly;
 
 public class Pond {
     public static void main(String[] args){
+        //operations
+        Frog.setSpecies("1331 Frogs");
+
         //Frogs
         Frog Peepo = new Frog("Peepo");
         Frog Pepe = new Frog("Pepe",10, 15);
@@ -15,9 +18,6 @@ public class Pond {
         Fly Fly1 = new Fly(1, 3);
         Fly Fly2 = new Fly(6);
         Fly Fly3 = new Fly(2, 4);
-
-        //operations
-        Clyde.setSpecies("1331 Frogs");
 
         System.out.println(Peepo.toString());
         Peepo.eat(Fly2);
